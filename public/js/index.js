56d60f4a14c522884b3d95efdb410ab2eb6ca45f
Judy Mou <moujudy@gmail.com>
2016-02-07 13:20:26 -0800

    donation percentage working

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,10 @@ function App() {
 
   App.prototype.init = function() { 
     window.randomize = function() {
-      $('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+      var amountRaised = parseInt($('#amountRaised').text());
+      var amountLeft = parseInt($('#amountLeft').text());
+      $('.radial-progress').attr('data-progress',
+          Math.floor(amountRaised / (amountRaised + amountLeft) * 100));
     }
     setTimeout(window.randomize, 200);
     $('.radial-progress').click(window.randomize);
