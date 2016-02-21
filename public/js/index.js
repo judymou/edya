e5844c95d8426305c6b444688a6c21519c2e92d6
Judy Mou <moujudy@gmail.com>
2016-02-21 01:37:32 -0800

    stripe working

--- public/js/index.js
+++ public/js/index.js
@@ -2,38 +2,56 @@ function App() {
   var me = this;
 
   App.prototype.init = function() {
-    window.randomize = function() {
-      var amountRaised = parseInt($('#amountRaised').text());
-      var amountLeft = parseInt($('#amountLeft').text());
-      $('.radial-progress').attr('data-progress',
-          Math.floor(amountRaised / (amountRaised + amountLeft) * 100));
-    }
-    setTimeout(window.randomize, 200);
-    $('.radial-progress').click(window.randomize);
+    setTimeout(me._loadProgress, 200);
+    $('.radial-progress').click(me._loadProgress);
+
+    var handler = StripeCheckout.configure({
+      key: 'pk_test_TFHGrWKWYdgVzsv5eLm3PW9C',
+      token: function(token) {
+        document.body.style.cursor = 'wait';
+        $.get('/pay?email=' + token.email + '&tok=' + token.id + '&amount=' +
+          $('#amount').val() * 100 + '&project=' + window.location.pathname, function(resp) {
+            document.body.style.cursor='default';
+            if (resp === 'ok') {
+              setTimeout(me._updateProgress, 200);
+              $('.radial-progress').click(me._updateProgress);
+            } else {
+              window.alert('Uh oh, something went wrong. Your card was not charged.');
+            }
+          });
+      },
+    });
+
+    $('#give-button').on('click', function() {
+      handler.open({
+        name:'Edya',
+        amount: $('#amount').val() * 100,
+      });
+    });
   };
 
-  var handler = StripeCheckout.configure({
-    key: 'pk_test_TFHGrWKWYdgVzsv5eLm3PW9C',
-    token: function(token) {
-      document.body.style.cursor = 'wait';
-      $.get('/pay?email=' + token.email + '&tok=' + token.id + '&amount=' +
-        $('#amount').val() * 100 + '&project=' + window.location.pathname, function(resp) {
-          document.body.style.cursor='default';
-          if (resp === 'ok') {
-            window.alert("Thanks! We'll follow up with confirmation and updates of your donation.");
-          } else {
-            window.alert('Uh oh, something went wrong. Your card was not charged.');
-          }
-        });
-    },
-  });
+  App.prototype._loadProgress = function() {
+    var amountRaised = parseInt($('#amountRaised').text());
+    var amountLeft = parseInt($('#amountLeft').text());
+    me._loadProgressBar(amountRaised, amountLeft);
+  }
 
-  $('#give-button').on('click', function() {
-    handler.open({
-      name:'Edya',
-      amount: $('#amount').val() * 100,
-    });
-  });
+  App.prototype._updateProgress = function() {
+    var amountRaised = parseInt($('#amountRaised').text()) + parseInt($('#amount').val());
+    var amountLeft = parseInt($('#amountLeft').text()) - parseInt($('#amount').val());
+    $('#amountRaised').text(amountRaised);
+    $('#amountLeft').text(amountLeft);
+    $('#numDonor').text(parseInt($('#numDonor').text()) + 1);
+    $('#amount').val("");
+    me._loadProgressBar(amountRaised, amountLeft);
+    $('.thankyou').css('opacity', '100');
+    $('.thankyou').css('height', 'auto');
+  }
+
+  App.prototype._loadProgressBar = function(amountRaised, amountLeft) {
+    $('.radial-progress').attr('data-progress',
+        Math.floor(amountRaised / (amountRaised + amountLeft) * 100));
+  }
 }
 $(function() {
   window.app = new App();
