7994ea30e3d48bfc840aa1f21912a3b6c98afd96
Judy Mou <moujudy@gmail.com>
2016-02-20 22:19:28 -0800

    stripe client side

--- public/js/index.js
+++ public/js/index.js
@@ -11,6 +11,29 @@ function App() {
     setTimeout(window.randomize, 200);
     $('.radial-progress').click(window.randomize);
   };
+
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_TFHGrWKWYdgVzsv5eLm3PW9C',
+    token: function(token) {
+      document.body.style.cursor = 'wait';
+      $.get('/pay?email=' + token.email + '&tok=' + token.id + '&amount=' +
+        $('#amount').val() * 100, function(resp) {
+          document.body.style.cursor='default';
+          if (resp === 'ok') {
+            window.alert("Thanks! We'll follow up with confirmation and updates of your donation.");
+          } else {
+            window.alert('Uh oh, something went wrong. Your card was not charged.');
+          }
+        });
+    },
+  });
+
+  $('#give-button').on('click', function() {
+    handler.open({
+      name:'Edya',
+      amount: $('#amount').val() * 100,
+    });
+  });
 }
 $(function() {
   window.app = new App();
