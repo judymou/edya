a55d3f3f5996fb0906e6b1ca976e476b39eab117
Judy Mou <moujudy@gmail.com>
2016-03-24 00:48:27 -0700

    update filter logic

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,80 +1,90 @@
-$(function() {
-  var items = [
-    ['extracurricular reading', 'classroom content support', 'classroom experience'],
-    ['primary school', 'middle school', 'high school', 'university'],
-    ['computers', 'tablets', 'eReaders', 'phones'],
-    ['no internet', 'infrequent', 'stable'],
-    ['free']
+function App() {
+  var me = this;
+  App.prototype.init = function () {
+    var items = [
+      ['extracurricular reading', 'classroom content support', 'classroom experience'],
+      ['primary school', 'middle school', 'high school', 'university'],
+      ['computer', 'tablet', 'eReader', 'phone'],
+      ['no internet', 'infrequent', 'stable'],
+      ['free']
+      ];
+    var tags = [
+      'Goals', 'Grade', 'Device', 'Internet', 'Cost'
     ];
-  var tags = [
-    'Goals', 'Grade', 'Device', 'Internet', 'Cost'
-  ];
-  for (var i = 0; i < tags.length; i++) {
-    $('.' + tags[i].toLowerCase().replace(/ /g, "_")).append('<h4>' + tags[i] + '</h4>');
-    for (var j = 0; j < items[i].length; j++) {
-      var label = $('<label class="active">');
-      label.append($('<input type="checkbox" checked/>').attr('id', items[i][j].toLowerCase().replace(/ /g, '_')));
-      label.append(items[i][j]);
-      $('.' + tags[i].toLowerCase().replace(/ /g, "_")).append(label);
-    }
-  }
-
-  $('.filters input').click(function() {
-    $(this).parent().toggleClass('active');
-    var count = 0;
     for (var i = 0; i < tags.length; i++) {
+      $('.' + tags[i].toLowerCase().replace(/ /g, "_")).append('<h4>' + tags[i] + '</h4>');
       for (var j = 0; j < items[i].length; j++) {
-        items[i][j] = items[i][j].toLowerCase().replace(/ /g, '_');
-        if ($('#' + items[i][j]).prop('checked')) {
-          count++;
+        var label = $('<label>');
+        label.append($('<input type="checkbox"/>').attr('id', items[i][j].toLowerCase().replace(/ /g, '_')));
+        label.append(items[i][j]);
+        $('.' + tags[i].toLowerCase().replace(/ /g, "_")).append(label);
+      }
+    }
+
+    $('.filters input').click(function() {
+      $(this).parent().toggleClass('active');
+      var count = 0;
+      for (var i = 0; i < tags.length; i++) {
+        for (var j = 0; j < items[i].length; j++) {
+          items[i][j] = items[i][j].toLowerCase().replace(/ /g, '_');
+          if ($('#' + items[i][j]).prop('checked')) {
+            count++;
+          }
         }
       }
-    }
 
-    if (count === items.length) {
-      // all selected
-      $('.resource').show();
-      return;
-    }
-    $('.resource').hide();
-    $('.resource').each(function(p, item) {
-      var place = $(item);
-      var txt = place.text().toLowerCase();
-      var checkedItems = [];
-      if ($('#primary_school').prop('checked')
-          && $('#middle_school').prop('checked')
-          && $('#high_school').prop('checked')) {
-        checkedItems.push('all grades');
-      } else {
-        checkedItems = checkedItems.concat(items[1]);
+      if (count === items.length) {
+        // all selected
+        $('.resource').show();
+        return;
       }
-      if ($('#computers').prop('checked')
-          && $('#tablet').prop('checked')
-          && $('#eReaders').prop('checked')
-          && $('#phones').prop('checked')) {
-        checkedItems.push('all devices');
-      } else {
-        checkedItems = checkedItems.concat(items[2]);
-      }
-      if ($('#no_internet').prop('checked')
-          && $('#infrequent').prop('checked')
-          && $('#stable').prop('checked')) {
-        checkedItems.push('all connection types');
-      } else {
-        checkedItems = checkedItems.concat(items[3]);
-      }
-      allCheckedItems = checkedItems.concat(items[0], items[4]);
-      for (var i = 0; i < allCheckedItems.length; i++) {
+      $('.resource').hide();
+      $('.resource').each(function(p, item) {
+        var place = $(item);
+        var txt = place.text().toLowerCase();
+        var checkItems = [];
+        checkItems = checkItems.concat(me._getCheckedItemsNoTag(items[0]));
+        checkItems = checkItems.concat(me._getCheckedItems(items[1], 'all grades'));
+        checkItems = checkItems.concat(me._getCheckedItems(items[2], 'all devices'));
+        checkItems = checkItems.concat(me._getCheckedItems(items[3], 'all connection types'));
+        checkItems = checkItems.concat(me._getCheckedItemsNoTag(items[4]));
         var passAllFilters = true;
-        if ($('#' + allCheckedItems[i]).prop('checked') && txt.indexOf(allCheckedItems[i].replace(/_/g, ' ')) > -1) {
-        } else {
-          passAllFilters = false;
-          break;
+        for (var i = 0; i < checkItems.length; i++) {
+          if ($('#' + checkItems[i]).prop('checked') && txt.indexOf(checkItems[i].replace(/_/g, ' ')) < 0) {
+            passAllFilters = false;
+            break;
+          }
         }
         if (passAllFilters) {
           place.show();
         }
-      }
+      });
     });
-  });
+  };
+
+  App.prototype._getCheckedItems = function(a, newTag) {
+    var itemChecked = [];
+    for (var i = 0; i < a.length; i++) {
+      if ($('#'+a[i]).prop('checked')) {
+        itemChecked.push(a[i]);
+      }
+    }
+    if (itemChecked.length == a.length) {
+      return [newTag];
+    }
+    return itemChecked;
+  }
+  App.prototype._getCheckedItemsNoTag = function(a) {
+    var itemChecked = [];
+    for (var i = 0; i < a.length; i++) {
+      if ($('#'+a[i]).prop('checked')) {
+        itemChecked.push(a[i]);
+      }
+    }
+    return itemChecked;
+  }
+}
+$(function() {
+  window.app = new App();
+  window.app.init();
 });
